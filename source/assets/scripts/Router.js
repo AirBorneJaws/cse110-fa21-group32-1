49bc30f5e1e6e2164ee0be04cd150ae984b26888
Esther <c3qiu@ucsd.edu>
2021-11-11 03:44:49 -0800

    some part of the router, still need to work on it

--- source/assets/scripts/Router.js
+++ source/assets/scripts/Router.js
@@ -1,8 +1,37 @@
 // router.js
-
  export class Router {
     static routes = {};
-
+    constructor(homeFunc) {
+        this['home'] = homeFunc;
+      }
+    
+      addPage(page, pageFunc) {
+        this[page] = pageFunc;
+       }
+    
+      navigate(page, statePopped) {
+        console.log(`navigate() function called, requested page: ${page}`);
+  
+        if(!this[page]){
+          console.log("Function not exist.");
+          return;
+        }
+  
+        var hash;
+        if(page == 'home'){
+          hash = "";
+        }
+        else{
+          hash = '#' + page;
+        }
+  
+        if(!statePopped && (window.location.hash != hash)){
+          history.pushState({page}, " ", window.location + hash );
+        }
+    
+  
+        this[page]();
+      }
 
   
   }
---
+++ source/assets/scripts/main.js
@@ -0,0 +1,49 @@
+import { Router } from './Router.js';
+
+const recipes = [
+    'https://introweb.tech/assets/json/ghostCookies.json',
+    'https://introweb.tech/assets/json/birthdayCake.json',
+    'https://introweb.tech/assets/json/chocolateChip.json',
+    'https://introweb.tech/assets/json/stuffing.json',
+    'https://introweb.tech/assets/json/turkey.json',
+    'https://introweb.tech/assets/json/pumpkinPie.json'
+  ];
+
+const recipeData = {}
+
+const router = new Router(function () {
+    document.getElementsByClassName('calendar').classList.add('shown');
+    document.getElementsByClassName('calendar').classList.remove('shown');
+
+    document.getElementsByClassName('grocery-list').classList.add('shown');
+    document.getElementsByClassName('grocery-list').classList.remove('shown');
+
+    document.getElementsByClassName('recipe').classList.add('shown');
+    document.getElementsByClassName('recipe').classList.remove('shown');
+
+    document.getElementsByClassName('cooking-mode').classList.add('shown');
+    document.getElementsByClassName('cooking-mode').classList.remove('shown');
+
+    document.getElementsByClassName('profile-page').classList.add('shown');
+    document.getElementsByClassName('profile-page').classList.remove('shown');
+
+    document.getElementsByClassName('favorites').classList.add('shown');
+    document.getElementsByClassName('favorites').classList.remove('shown');
+
+    document.getElementsByClassName('add-recipe').classList.add('shown');
+    document.getElementsByClassName('add-recipe').classList.remove('shown');
+});
+
+window.addEventListener('DOMContentLoaded', init);
+
+
+async function init() {
+    initializeServiceWorker();
+  
+    try {
+      await fetchRecipes();
+    } catch (err) {
+      console.log(`Error fetching recipes: ${err}`);
+      return;
+    }
+}
